66a2dd8e5e608cb853e325581b41583934859c95
fatkulnurk <fatkulnurk@gmail.com>
2019-03-03 08:11:19 +0700

    merubah halaman create post dan page

--- resources/views/dashblog/page/add.blade.php
+++ resources/views/dashblog/page/add.blade.php
@@ -1,73 +1,94 @@
 @extends('layouts.dashblog')
 
-@section('title', 'Dashboard')
+@section('title', 'Add New Page')
 
 @section('content')
-    <section class="section">
-        <div class="section-header">
-            <h1>Tambah Halaman</h1>
-            <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="#">usernameblog.domain.com</a></div>
-            </div>
-        </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Tambah Halaman Baru</h4>
+                </div>
+                <div class="card-body">
+                    <div class="form-group mb-4">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="col-form-label">Title</label>
+                                <input type="text" class="form-control" placeholder="type title here">
+                            </div>
 
-        <div class="section-body">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Tambah Halaman Baru</h4>
+                            <div class="col-md-6">
+                                <label class="col-form-label">Status Page</label>
+                                <select class="form-control selectric">
+                                    @foreach(\App\Enum\StatusPageEnum::getAll() as $key => $value)
+                                        <option value="{{ $key }}">{{ $value }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <div class="form-group row mb-4">
-                                <label class="col-form-label text-md-right col-12 col-md-1">Title</label>
-                                <div class="col-sm-12 col-md-11">
-                                    <input type="text" class="form-control" placeholder="type title here">
-                                </div>
+                    </div>
+                    <div class="form-group mb-4">
+                        <label class="col-form-label">Content</label>
+                        <textarea name="content" class="form-control my-editor"></textarea>
+                    </div>
+                    <div class="form-group mb-4">
+                        <div class="row">
+                            <div class="col-6">
+                                <button class="btn btn-primary btn-block">Submit</button>
                             </div>
-                            <div class="form-group row mb-4">
-                                <label class="col-form-label text-md-right col-12 col-md-1">Content</label>
-                                <div class="col-sm-12 col-md-11">
-                                    <textarea id="summernote" class="summernote"></textarea>
-                                </div>
-                            </div>
-                            <div class="form-group row mb-4">
-                                <div class="col-sm-12 col-md-12">
-                                    <button class="btn btn-primary">Submit</button>
-                                    <button class="btn btn-default">Back</button>
-                                </div>
+                            <div class="col-6">
+                                <button class="btn btn-light btn-block">Cancel</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+
 @endsection
 @push('push-head')
-    <link rel="stylesheet" href="{{ asset('assets/modules/summernote/summernote-bs4.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/modules/codemirror/lib/codemirror.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/modules/codemirror/theme/duotone-dark.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/modules/jquery-selectric/selectric.css') }}">
 @endpush
 @push('push-footer')
-    <script src="{{ asset('assets/modules/summernote/summernote-bs4.js') }}"></script>
-    <script src="{{ asset('assets/modules/codemirror/lib/codemirror.js') }}"></script>
-    <script src="{{ asset('assets/modules/codemirror/mode/javascript/javascript.js') }}"></script>
     <script src="{{ asset('assets/modules/jquery-selectric/jquery.selectric.min.js') }}"></script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.9.3/tinymce.min.js"></script>
     <script>
-        $('#summernote').summernote({
-            toolbar: [
-                ['style', ['bold', 'italic', 'underline', 'clear']],
-                ['font', ['strikethrough', 'superscript', 'subscript']],
-                ['fontsize', ['fontsize']],
-                ['color', ['color']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['height', ['height']]
+        var editor_config = {
+            branding: false,
+            height: 320,
+            path_absolute : "/",
+            selector: "textarea.my-editor",
+            plugins: [
+                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                "searchreplace wordcount visualblocks visualchars code fullscreen",
+                "insertdatetime media nonbreaking save table contextmenu directionality",
+                "emoticons template paste textcolor colorpicker textpattern"
             ],
-            dialogsInBody: true,
-            minHeight: 250,
-        });
+            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+            relative_urls: false,
+            file_browser_callback : function(field_name, url, type, win) {
+                var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+                var cmsURL = editor_config.path_absolute + 'filemanager?field_name=' + field_name;
+                if (type == 'image') {
+                    cmsURL = cmsURL + "&type=Images";
+                } else {
+                    cmsURL = cmsURL + "&type=Files";
+                }
+
+                tinyMCE.activeEditor.windowManager.open({
+                    file : cmsURL,
+                    title : 'Filemanager',
+                    height : y * 0.8,
+                    resizable : "yes",
+                    close_previous : "no"
+                });
+            }
+        };
+
+        tinymce.init(editor_config);
     </script>
 @endpush
--- resources/views/dashblog/post/add.blade.php
+++ resources/views/dashblog/post/add.blade.php
@@ -3,80 +3,86 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <section class="section">
-        <div class="section-header">
-            <h1>Tambah Postingan</h1>
-            <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="#">usernameblog.domain.com</a></div>
-            </div>
-        </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Tambah Postingan Baru</h4>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="form-group mb-4">
+                                <label class="col-form-label">Title</label>
+                                <input type="text" class="form-control" placeholder="type title here">
+                            </div>
 
-        <div class="section-body">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Tambah Postingan Baru</h4>
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-8">
-                                    <div class="form-group mb-4">
-                                        <label class="col-form-label">Title</label>
-                                        <input type="text" class="form-control" placeholder="type title here">
-                                    </div>
-
-                                    <div class="form-group mb-4">
+                            <div class="form-group mb-4">
+                                <div class="row">
+                                    <div class="col-md-6">
                                         <label class="col-form-label">Category</label>
                                         <select class="form-control selectric">
-                                            <option>Tech</option>
-                                            <option>News</option>
-                                            <option>Political</option>
+                                            @foreach($category as $cat)
+                                                <option value="{{ $cat->id }}">{{ $cat->name }}</option>
+                                            @endforeach
                                         </select>
                                     </div>
+                                    <div class="col-md-6">
+                                        <label class="col-form-label">Status Post</label>
+                                        <select class="form-control selectric">
+                                            @foreach(\App\Enum\StatusPostEnum::getAll() as $key => $value)
+                                                <option value="{{ $key }}">{{ $value }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
 
-                                    <div class="form-group mb-4">
-                                        <label class="col-form-label">Tags</label>
-                                        <input type="text" class="form-control inputtags">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group mb-4">
-                                        <label class="col-form-label">Thumbnail</label>
-                                        <div id="image-preview" class="image-preview">
-                                            <label for="image-upload" id="image-label">Choose File</label>
-                                            <input type="file" name="image" id="image-upload" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                             <div class="form-group mb-4">
-                                <label class="col-form-label">Content</label>
-                                <textarea id="summernote" class="summernote"></textarea>
-                            </div>
-                            <div class="form-group mb-4">
-                                <button class="btn btn-primary">Publish</button>
-                                <button class="btn btn-info">Save Draft</button>
-                                <button class="btn btn-default">Cancel</button>
+                                <label class="col-form-label">Tags</label>
+                                <input type="text" class="form-control inputtags">
                             </div>
                         </div>
-                        <div class="card-footer">
-
+                        <div class="col-md-4">
+                            <div class="form-group mb-4">
+                                <label class="col-form-label">Thumbnail</label>
+                                <div id="image-preview" class="image-preview">
+                                    <label for="image-upload" id="image-label">Choose File</label>
+                                    <input type="file" name="image" id="image-upload" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group mb-4">
+                        <label class="col-form-label">Content</label>
+                        <textarea name="content" class="form-control my-editor"></textarea>
+                    </div>
+                    <div class="form-group mb-4">
+                        <div class="row">
+                            <div class="col-6">
+                                <button class="btn btn-primary btn-block">Submit</button>
+                            </div>
+                            <div class="col-6">
+                                <button class="btn btn-light btn-block">Cancel</button>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="card-footer">
+
+                </div>
             </div>
         </div>
-    </section>
+    </div>
+
 @endsection
 @push('push-head')
-    <link rel="stylesheet" href="{{ asset('assets/modules/summernote/summernote-bs4.css') }}">
+
     <link rel="stylesheet" href="{{ asset('assets/modules/jquery-selectric/selectric.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.css') }}">
+
 @endpush
 @push('push-footer')
-    <script src="{{ asset('assets/modules/summernote/summernote-bs4.js') }}"></script>
     <script src="{{ asset('assets/modules/jquery-selectric/jquery.selectric.min.js') }}"></script>
 
 
@@ -85,19 +91,43 @@
 
     <!-- Page Specific JS File -->
     <script src="{{ asset('assets/js/page/features-post-create.js') }}"></script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.9.3/tinymce.min.js"></script>
     <script>
-        $('#summernote').summernote({
-            toolbar: [
-                ['style', ['bold', 'italic', 'underline', 'clear']],
-                ['font', ['strikethrough', 'superscript', 'subscript']],
-                ['fontsize', ['fontsize']],
-                ['color', ['color']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['height', ['height']],
-                ['misc', ['fullscreen']]
+        var editor_config = {
+            branding: false,
+            height: 320,
+            path_absolute : "/",
+            selector: "textarea.my-editor",
+            plugins: [
+                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                "searchreplace wordcount visualblocks visualchars code fullscreen",
+                "insertdatetime media nonbreaking save table contextmenu directionality",
+                "emoticons template paste textcolor colorpicker textpattern"
             ],
-            dialogsInBody: true,
-            minHeight: 250,
-        });
+            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+            relative_urls: false,
+            file_browser_callback : function(field_name, url, type, win) {
+                var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+                var cmsURL = editor_config.path_absolute + 'filemanager?field_name=' + field_name;
+                if (type == 'image') {
+                    cmsURL = cmsURL + "&type=Images";
+                } else {
+                    cmsURL = cmsURL + "&type=Files";
+                }
+
+                tinyMCE.activeEditor.windowManager.open({
+                    file : cmsURL,
+                    title : 'Filemanager',
+                    height : y * 0.8,
+                    resizable : "yes",
+                    close_previous : "no"
+                });
+            }
+        };
+
+        tinymce.init(editor_config);
     </script>
 @endpush
