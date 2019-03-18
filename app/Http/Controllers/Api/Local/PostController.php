5fe9aa441eb7aa4c8959eb09e5a33ed4bb17ef72
fatkulnurk <fatkulnurk@gmail.com>
2019-03-18 09:07:26 +0700

    memperbaiki api local

--- app/Http/Controllers/Api/Local/PostController.php
+++ app/Http/Controllers/Api/Local/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     public function index() {
 //        $post = new PostCollection(Post::paginate(10));
 
-        $post = Post::select('id', )->paginate(10);
+        $post = Post::orderBy('created_at', 'desc')->paginate(10);
 
         return $post;
     }
